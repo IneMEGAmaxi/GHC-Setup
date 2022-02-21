2681ee4ece9e8d185cf5e7cef47ed4401e7fa226
Joey De Pauw <joeydepauw@gmail.com>
2022-02-21 19:36:40 +0100

    basic graph code

--- src/util.py
+++ src/util.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 import numpy as np
 
 import scipy.sparse
@@ -47,3 +47,73 @@ def vector_to_list(v, uniques):
     """ Converts a single binary vector back to a list of items according to index of uniques """
     indices = v.nonzero()[0]
     return [uniques[i] for i in indices]
+
+
+#################
+#   Graphs      #
+#################
+
+from graphviz import Digraph
+
+
+class Graph:
+    def __init__(self):
+        self.nodes = dict()
+
+    def addNode(self, name):
+        """ Force add node """
+        assert name not in self.nodes
+        self.nodes[name] = Node(self, name)
+
+    def getNode(self, name):
+        """ Get or create node. """
+        if name not in self.nodes:
+            self.nodes[name] = Node(self, name)
+        return self.nodes[name]
+
+    def __str__(self):
+        return "\n".join([str(node) for node in self.nodes.values()])
+
+    def toDot(self):
+        # other shape: plaintext
+        dot = Digraph(node_attr={'shape': 'circle'})
+        for node in self.nodes.values():
+            node.toDot(dot)
+        return dot
+
+
+class Node:
+    def __init__(self, graph: Graph, name: str):
+        self.graph = graph
+        self.name = name
+        # edges pointing away from node (reading allowed, for writing use functions)
+        self.outgoing = dict()
+        # edges pointing to node (reading allowed, for writing use functions)
+        self.incoming = dict()
+
+    def addOutgoing(self, node: Union[str, 'Node'], data=None):
+        if isinstance(node, str):
+            node = self.graph.getNode(node)
+        if not node in self.outgoing:
+            self.outgoing[node] = data
+            node.addIncoming(self, data)
+
+    def addIncoming(self, node: Union[str, 'Node'], data=None):
+        if isinstance(node, str):
+            node = self.graph.getNode(node)
+        if not node in self.incoming:
+            self.incoming[node] = data
+            node.addOutgoing(self, data)
+
+    def __str__(self):
+        s = self.name + '\n'
+        for node, edge in self.outgoing.items():
+            s += f"->{node.name} ({repr(edge)})\n"
+        for node, edge in self.incoming.items():
+            s += f"<-{node.name} ({repr(edge)})\n"
+        return s
+
+    def toDot(self, dot):
+        dot.node(self.name)
+        for other, data in self.outgoing.items():
+            dot.edge(self.name, other.name, str(data))
